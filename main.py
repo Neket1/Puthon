b14102f6e106c08ca194d4e8bb949f5d4c0f9c0f
Neket1 <vasa2018.neket.33.terentyev.45@gmail.com>
2024-06-30 12:17:19 +0300

    практическое задание сделано

---
+++ main.py
@@ -0,0 +1,8 @@
+print("1st program")
+print((5**0.5)*5)
+print("2st program")
+print(bool(9.99 > 9.98 and 1000 != 1000.1))
+print("3st program")
+print(((5678 // 10) % 100) + ((1234 // 10) % 100))
+print("4st program")
+print(13 == 13 and 42 == 42) # не уверен что так
