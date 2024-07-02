a6f16ac84941698cd10e4e4be602bb145dc306e3
Neket1 <vasa2018.neket.33.terentyev.45@gmail.com>
2024-07-02 10:45:15 +0300

    new DZ

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-print("1st program")
-print((5**0.5)*5)
-print("2st program")
-print(bool(9.99 > 9.98 and 1000 != 1000.1))
-print("3st program")
-print(((5678 // 10) % 100) + ((1234 // 10) % 100))
-print("4st program")
-print(13 == 13 and 42 == 42) # не уверен что так
+name = "Топинамбур"
+print(name[0])
+print(name[-1])
+print(name[5:])
+print(name[::-1])
+print(name[1::2])
