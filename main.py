fc8c8859a79b7558d0c9dc8236ee8c03575e32a6
Neket1 <vasa2018.neket.33.terentyev.45@gmail.com>
2024-07-06 15:15:45 +0300

    new_DZ_Perem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-name = "Топинамбур"
-print(name[0])
-print(name[-1])
-print(name[5:])
-print(name[::-1])
-print(name[1::2])
+perm1 = 12
+perm2 = 1.5
+perm3 = "Puthon"
+print(perm3,"всего задач:", perm1,"затрачено часов:", perm2, "среднее время выполнения",(perm2/perm1))
+
