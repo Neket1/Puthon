f40b7ca0d34763afdf593bec01a699234de4a758
Neket1 <vasa2018.neket.33.terentyev.45@gmail.com>
2024-07-06 15:29:49 +0300

    new_DZ

--- main.py
+++ main.py
@@ -2,4 +2,3 @@ perm1 = 12
 perm2 = 1.5
 perm3 = "Puthon"
 print(perm3,"всего задач:", perm1,"затрачено часов:", perm2, "среднее время выполнения",(perm2/perm1))
-
---
+++ new_dinam_type.py
@@ -0,0 +1,8 @@
+name = "Никита"
+print("Name", name)
+age = 19
+print("age", age)
+new_age = 20
+print("New age", new_age)
+is_student = True
+print("Is student", is_student)
