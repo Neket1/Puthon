d4a02fa09ec356a953a23b864b2a2eea716ffc73
Neket1 <vasa2018.neket.33.terentyev.45@gmail.com>
2024-07-10 21:29:33 +0300

    Update Kotdg_obj.py

--- Kotdg_obj.py
+++ Kotdg_obj.py
@@ -1,6 +1,10 @@
-immutable_var = 1, 23, 54, True, "string"
+#___________________________________________кортеж
+immutable_var = ([1, 4, 2], 0, True, "string")
 print(immutable_var)# кортеж не изменяимый в отличии от списка
-mutable_list = ([1, 4, 2], 0) # но если сделать так он будет изменяемый
+immutable_var[0][0] = 53 
+print(immutable_var)
+#___________________________________________список
+mutable_list = [0,4,12,4,2,4,6,2]
 print(mutable_list)
-mutable_list[0][0] = 53
+mutable_list[0] = 9999
 print(mutable_list)
