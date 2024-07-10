c57fc38f8cecf91cbd2906883dc5f01fb9136803
Neket1 <vasa2018.neket.33.terentyev.45@gmail.com>
2024-07-10 21:55:25 +0300

    new_model_quest

---
+++ modul_quest.py
@@ -0,0 +1,9 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+stud = sorted(students)# да есть вариант сделать так
+print(stud[0], sum(grades[0])/5)
+print(stud[1], sum(grades[1])/5)
+print(stud[2], sum(grades[2])/5)
+print(stud[3], sum(grades[3])/5)
+print(stud[4], sum(grades[4])/5)
+#или попробовать это сделать через For но не уверен зачтете ли это
