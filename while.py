226a449a124fd42057c9fe7eaff5929ea3e75a7b
Neket1 <vasa2018.neket.33.terentyev.45@gmail.com>
2024-08-03 23:05:47 +0300

    new_dz

--- while.py
+++ while.py
@@ -1,9 +1,12 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
-while int(my_list[i]) != 0:
-    if int(my_list[i]) != 0 and int(my_list[i]) > 0:
+while my_list[1] >= my_list[0]:
+    if int(my_list[i]) > 0:
         print(my_list[i])
         i += 1
+    elif int(my_list[i]) == 0:
+        my_list.remove(my_list[i])
+
     else:
         print('остановка цикла')
         break
